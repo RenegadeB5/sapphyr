ee5dac177cb06ac19f522bd55671a01b2dbd203b
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-05 16:48:52 -0400

    Update artchannel.js

--- services/artchannel.js
+++ services/artchannel.js
@@ -1,12 +1,12 @@
 module.exports = {
-	name: "Art Channel Service",
-	description: "Reacts with an upvote on artwork submissions.",
-	type: "event",
-	on: {
-		message: async function (message) {
-			if (message.channel.id === message.guild.channels.find("name", "art").id & message.attachments.size > 0)
-				console.log('test');
-				message.react("🔺");
-		}
-	}
+        name: "Art Channel Service",
+        description: "Reacts with an upvote on artwork submissions.",
+        type: "event",
+        on: { 
+                message: async function (message) {
+                        if (message.channel.id === message.guild.channels.find("name", "art").id & message.attachments.size > 0)
+                                console.log('test');
+                                message.react("🔺");
+                        }
+                }
 };
