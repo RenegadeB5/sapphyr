6294abbf36f3e243050e890066ec4a2058155415
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-05 15:15:43 -0400

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -13,7 +13,7 @@ module.exports = {
 			if (diepregex.test(args[0])) {
 				let link = args[0];
 				let notes = args.slice(1).join(" ");
-				let linkchannel = this.msg.guild.channels.find("name", "member-links")
+				let linkchannel = message.guild.channels.find("name", "member-links")
                                 let embed = new Discord.RichEmbed()
                                 .setColor(0x00FF00)
                                 .setFooter('React with \"🔗\" to get the link.')
