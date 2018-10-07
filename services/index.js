6f08e5a95d4d0724d83614ec48c3719f4518b6bc
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-06 23:23:51 -0400

    Update index.js

--- services/index.js
+++ services/index.js
@@ -47,12 +47,19 @@ let events = [
 	"webhookUpdate"
 ];
 
+function callServiceEvent(event, args) {
+	services.forEach(service => {
+		if (typeof service.type === "undefined") return;
+		if (typeof service.type === "string" && service.type !== "event") return;
+		if (Array.isArray(service.type) && (service.type.includes("event") === false)) return;
+		if (typeof service.on[event] !== "undefined")
+			service.on[event](...args);
+	});
+}
 
 exports.initializeServices = async client => await Promise.all(services.map(async service => {
 	if (typeof service.initialize !== "undefined")
 		await service.initialize(client);
-	for (let event of events)
-		client.on(event, (...args) => callServiceEvent(event, args));
 }));
 
 exports.services = services;
