75143b6774cf195d709be29171a40b13a28aa19a
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-07 17:00:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var path = require("path");
 var config = require("./localdata/config.json");
 var commando = require("discord.js-commando");
 var MongoClient = require('commando-provider-mongo').MongoClient;
-var MongoDBProvider = require('commando-provider-mongo');
+var MongoDBProvider = require('mongodb');
 var { initializeServices, services } = require("./services");
 
 var client = new commando.Client({
