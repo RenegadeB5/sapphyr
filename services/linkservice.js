60ef1d98bec791b02ac3ecf968d1e5d0185979ce
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-07 03:35:02 -0400

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -1,11 +1,13 @@
 module.exports = {
-	name: "Art Channel Service",
-	description: "Reacts with an upvote on artwork submissions.",
+	name: "Link sending service",
+	description: "When a user reacts with \"🔗\", they will be sent a party link.",
 	type: "event",
 	on: {
-		message: async function (message) {
-			if (message.channel.id === '496832031259492362' && message.attachments.size > 0)
-				message.react("🔺");
+		messageReactionAdd: async function (reaction) {
+			if(reaction.emoji.name === '🔗' && reaction.message.channel.id === '451417402119421952') {
+				const dm = reaction.users.map(r => r.id);
+				const dmsend = dm[dm.length-1];
+				client.users.get(dmsend).send('link');
 		}
 	}
 };
