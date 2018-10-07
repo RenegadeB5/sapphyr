365f328b8789e812fd5ff1fc19fae4fea4cd59db
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-07 03:03:16 -0400

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -8,21 +8,21 @@ module.exports = {
 	type: "event",
 	on: {
 		message: async function (message) {
-                        let notes = message.content.split(" ");
-                        let args = notes.slice(0); 
+			let notes = message.content.split(" ");
+			let args = notes.slice(0); 
                         if (diepregex.test(args[0])) {
                                 let link = args[0];
-                                let notes = args.slice(1).join(" ");
+				let notes = args.slice(1).join(" ");
                                 let linkchannel = client.channels.get('451417402119421952')
-                                let embed = new Discord.RichEmbed()
-                                .setColor(0x00FF00)
-                                .setTitle('Party invite')
+ 				let embed = new Discord.RichEmbed()
+				.setColor(0x00FF00)
+				.setTitle('Party invite')
                                 .addField('Notes', notes, true)
 				.addfield('Owner', message.member.user.tag, true)
                                 .setTimestamp()
 				.setFooter('React with \"🔗\" to get the link.')
-                                message.delete();
-                                linkchannel.send({embed}).then(function (message) {message.react('🔗')});
+				message.delete();
+				linkchannel.send({embed}).then(function (message) {message.react('🔗')});
 			}
 		}
 	}
