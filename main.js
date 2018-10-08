229a9ec1b6fdbaa6377abfdd2a89dd9e259b9786
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-07 21:04:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ var client = new commando.Client({
 	commandPrefix: "_"
 });
 
-var uri = "mongodb://RenegadeB5:puppies12@mycluster0-shard-00-00.mongodb.net:27017,mycluster0-shard-00-01.mongodb.net:27017,mycluster0-shard-00-02.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
+var uri = "mongodb://RenegadeB5:puppies12@cluster0-shard-00-00-l1qqw.mongodb.net:27017,cluster0-shard-00-01-l1qqw.mongodb.net:27017,cluster0-shard-00-02-l1qqw.mongodb.net:27017/admin?ssl=true&replicaSet=cluster0-shard-0&authSource=admin";
 MongoClient.connect(uri, function(err, client) {
 	if (err) {
 		console.error('An error occurred connecting to MongoDB: ', err);
