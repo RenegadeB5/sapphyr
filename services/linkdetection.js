5baaa7dcfe5d6ef9cf7030e196d59053481c7570
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-08 03:06:21 -0400

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -39,7 +39,7 @@ module.exports = {
 						const collection = client.db("partylinks").collection("links");
 						collection.find(query).toArray(function(err, linkname) {
 							if ('RenegadeBB#4311' === message.member.user.tag) {
-								console.log(linkname[1].name).catch(err);
+								console.log(linkname[0]).catch(err);
 								message.channel.send('You already have a link posted. Use the clearlink command to remove it.').then(message => {message.delete(5000)});
 							}
 							else {
