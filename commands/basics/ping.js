7a61a6c5411ba3b40e4f4222b9921c4d1c8fc810
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-08 01:42:07 -0400

    Update ping.js

--- commands/basics/ping.js
+++ commands/basics/ping.js
@@ -1,28 +1,16 @@
 var { Command } = require("discord.js-commando");
-module.exports = class ClearLinkCommand extends Command {
+module.exports = class PingCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "clearlink",
-            description: "Removes the users current party link from the database",
+            name: "ping",
+            description: "Checks the bot's ping.",
             group: "basics",
-            memberName: "clearlink"
+            memberName: "ping"
         });
     }
-    
+
     async run(msg) {
-        global.MongoClient.connect(global.uri, function(err, client) {
-            if (err) {
-                console.error('An error occurred connecting to MongoDB: ', err);
-			}
-			else {
-				const query = { name: msg.member.user.tag };
-				const collection = client.db("partylinks").collection("links");
-				collection.deleteOne(query, function(err, result) {
-				    console.log('link removed from db');
-                    msg.channel.send('Your link has successfully been removed from the database.').then(message => {message.delete(5000)});
-			    });
-		    	client.close();
-			}
-        });
+        let pingMsg = await msg.channel.send("🔁 | Pinging ...");
+        return await pingMsg.edit(`✅ | ${pingMsg.createdTimestamp - msg.createdTimestamp}ms.`);
     }
 };
