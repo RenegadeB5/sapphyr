82aa30e6d3441d7a923fab0f575fabda9030af14
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-08 02:29:20 -0400

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -38,7 +38,7 @@ module.exports = {
 						const insert = { name: message.member.user.tag, link: link };
 						const collection = client.db("partylinks").collection("links");
 						collection.find(query).toArray(function(err, linkname) {
-							if (linkname[0].link === link) {
+							if (1 + 1 === 3) {
 								message.channel.send('This link already exists.').then(message => {message.delete(10000)});
 							}
 							else {
