cd9dd68c2235be619d6bd8b07f58533c5105a3ee
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-10 21:26:45 -0400

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -1,3 +1,4 @@
+var Discord = require('discord.js');
 module.exports = {
 	name: "Link sending service",
 	description: "When a user reacts with \"🔗\", they will be sent a party link.",
@@ -7,28 +8,30 @@ module.exports = {
 			if(reaction.emoji.name === '🔗' && reaction.message.channel.id === '498736242905710592') {
 				const dm = reaction.users.map(r => r.id);
 				const dmsend = dm[dm.length-1];
-				if (dmsend === '407593823921766410') {
-					return;
-				}
-				else {
-					const log1 = reaction.users.map(r => r.username)
-					const log2 = reaction.users.map(r => r.discriminator)
-					const loguser = log1[log1.length-1] + '#' + log2[log2.length-1]
-					const messageid = reaction.users.map(r => r.lastMessageID);
-					global.MongoClient.connect(global.uri, function(err, client) {
-						if (err) {
-							console.error('An error occurred connecting to MongoDB: ', err);
-						}
-						else {
-							const query = { name: loguser };
-							const collection = client.db("partylinks").collection("links");
-							collection.find(query).toArray(function(err, result) {
-								global.client.users.get(dmsend).send(result[result.length-1].link);
-							});
-							client.close();
-						}
-					});
-				}
+				if (dmsend === '407593823921766410') return;
+				const name = reaction.users.map(r => r.username)
+				const discrim = reaction.users.map(r => r.discriminator)
+				const user = name[name.length-1] + '#' + discrim[discrim.length-1]
+				const messageid = reaction.users.map(r => r.lastMessageID);
+				global.MongoClient.connect(global.uri, function(err, client) {
+					if (err) {
+						console.error('An error occurred connecting to MongoDB: ', err);
+					}
+					else {
+						const query = { name: user };
+						const collection = client.db("partylinks").collection("links");
+						collection.find(query).toArray(function(err, result) {
+							global.client.users.get(dmsend).send(result[result.length-1].link);
+							let embed = new Discord.RichEmbed()
+							.setColor(0x0000FF)
+							.setTitle(user)
+							.addField('Party invite', notes)
+							.setFooter('React with 🔗 to get the link.')
+							reaction.channel.fetchMessage(messageid).then(message =>message.edit({embed})
+						});
+						client.close();
+					}
+				});
 			}
 		}
 	}
