fd9087521435a885d52d43e44540b52157197cfb
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-10 22:45:49 -0400

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -23,7 +23,7 @@ module.exports = {
 						const query = { name: user };
 						const collection = client.db("partylinks").collection("links");
 						collection.find(query).toArray(function(err, result) {
-							if (err) {
+							if (result[result.length-1].link === 'undefined') {
 								global.client.users.get(dmsend).send('Sorry, this invite link is no longer avalable.');
 								reaction.message.delete();
 							}
