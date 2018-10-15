d83e5eb4b9ed5faa07b5277fa7779ec1e9946c2c
RenegadeB5 <rocketmanB5@gmail.com>
2018-10-15 12:14:18 -0400

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -6,14 +6,14 @@ module.exports = {
 	on: {
 		messageReactionAdd: async function (reaction) {
 			if(reaction.emoji.name === '🔗' && reaction.message.channel.id === '448984516078600203') {
-				const dm = reaction.users.map(r => r.id);
-				const dmsend = dm[dm.length-1];
+				const user = reaction.users.map(r => r.id);
+				const userid = user[user.length-1];
 				const name = reaction.users.map(r => r.username)
 				const discrim = reaction.users.map(r => r.discriminator)
-				const user = name[name.length-1] + '#' + discrim[discrim.length-1]
+				const username = name[name.length-1] + '#' + discrim[discrim.length-1]
 				const list = reaction.message.embeds.map(r => r.fields.map(r => r.value))[0]
 				const members = list[1, list.length-1]
-				if (dmsend === '407593823921766410') return;
+				if (userid === '407593823921766410') return;
 				global.MongoClient.connect(global.uri, function(err, client) {
 					if (err) {
 						console.error('An error occurred connecting to MongoDB: ', err);
@@ -23,16 +23,16 @@ module.exports = {
 						const collection = client.db("partylinks").collection("links");
 						collection.find(query).toArray(function(err, result) {
 							if (result[result.length-1] === undefined) {
-								global.client.users.get(dmsend).send('Sorry, this invite link is no longer avalable.');
+								global.client.users.get(useeid).send('Sorry, this invite link is no longer avalable.');
 								reaction.message.delete();
 							}
 							else {
-								global.client.users.get(dmsend).send(result[result.length-1].link);
+								global.client.users.get(userid).send(result[result.length-1].link);
 								let embed = new Discord.RichEmbed()
 								.setColor(0x0000FF)
-								.setTitle(user)
+								.setTitle(username)
 								.addField('Party invite', result[result.length-1].notes)
-								.addField('Members', members + '\n' + user)
+								.addField('Members', members + '\n' + username)
 								.setFooter('React with 🔗 to get the link.')
 								reaction.message.edit({embed});  
 							}
