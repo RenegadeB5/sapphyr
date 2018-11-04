eabddb568bdb5f4d7d52e54a7d7b83acffd05841
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-04 15:46:00 -0500

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -13,8 +13,7 @@ module.exports = {
 			if (message.author.bot) return;
 			if (diepregex.test(args[0])) {
 				function clearLink () {
-  					console.log("1 link cleared");
-					new dataHandler().remove1Link();
+					await global.client.datahandler.remove1Link();
 				}
 				let link = args[0];
 				if (link.substr(0, 8) !== 'https://') {
@@ -25,16 +24,15 @@ module.exports = {
 					notes = 'No informtion provided.'
 				}
 				let linkchannel = client.channels.get('498736242905710592')
+				let insert = { name: message.member.user.tag, notes: notes, link: link }
  				let embed = new Discord.RichEmbed()
 				.setColor(0x0000FF)
 				.setTitle(message.member.user.tag)
 				.addField('Party invite', notes)
 				.addField('Members', message.member.user.tag)
 				.setFooter('React with 🔗 to recieve the link, \nReact with ☠ if the link is invalid, \n And react with ⚠ if there is a troller present. \n Be aware that false alarms are punishable.')
-				const insert = { name: message.member.user.tag, notes: notes, link: link };
 				await global.client.datahandler.insertLink(insert);
 				message.delete();
-				console.log("link added to db");
 				linkchannel.send({embed}).then(function (message) {message.react('🔗')});
 				message.channel.send('Your link has successfully been posted.').then(message => {message.delete(5000)});
 				setTimeout(clearLink, 3600000);
