5638526528f6a19c6fba090ceea4cae35a078bbf
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-03 21:44:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -16,51 +16,51 @@ var client = new commando.Client({
 
 client
 	.on("ready", async () => {
-		log(`Logged in as ${client.user.tag} (${client.user.id})`);
+		console.log(`Logged in as ${client.user.tag} (${client.user.id})`);
 		client.user.setActivity("with sapphires!");
 		// Initialize datahandler
 		client.datahandler = new utils.datahandler();
 		await client.datahandler.initialize();
-		log("Datahandler initialized.");
+		console.log("Datahandler initialized.");
 		// Initialize services
 		await initializeServices(client);
-		log("Services initialized.");
+		console.log("Services initialized.");
 		// Attach to global object
 		global.services = services;
 		global.utils = utils;
-		log("Global variables initialized.");
+		console.log("Global variables initialized.");
 		// Register commands, groups and types
 		client.registry.registerDefaultTypes();
-		log("Default types initialized.");
+		console.log("Default types initialized.");
 		client.registry.registerDefaultGroups();
-		log("Default groups initialized.");
+		console.log("Default groups initialized.");
 		client.registry.registerGroups([
 			["basics", "basic commands"],
 			["fun", "fun commands"],
 			["utils", "utility commands"],
 			["nadekoconnector", "nadekoconnector commands"]
 		]);
-		log("Command groups initialized.");
+		console.log("Command groups initialized.");
 		client.registry.registerCommandsIn(path.join(__dirname, "commands"));
-		log("Commands initialized.");
+		console.log("Commands initialized.");
 	})
 	.on("commandError", (cmd, err) => {
 		if (err instanceof commando.FriendlyError) return;
-		log.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+		console.log.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
 	})
 	.on("commandBlocked", (msg, reason) => {
-		log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
+		console.log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
 	})
 	.on("commandPrefixChange", (guild, prefix) => {
-		log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+		console.log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
 	})
 	.on("commandStatusChange", (guild, command, enabled) => {
-		log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+		console.log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
 	})
 	.on("groupStatusChange", (guild, group, enabled) => {
-		log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
+		console.log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
 	});
 
 client.login(process.env.BOT_TOKEN);
