f0073cc68cad61143f65865602e64516d334a8d7
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-03 22:31:52 -0400

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -29,7 +29,7 @@ module.exports = {
 								reaction.message.delete();
 							}
 							else {
-								if (members.includes(username) === 'true') {
+								if (members.includes(username) === true) {
 									return;
 								}
 								else {
