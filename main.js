cd584319c6d1e0ffa56bc4f7beb26190e817318d
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-04 10:50:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-var path = require("path");
-var config = require("./localdata/config.json");
-var commando = require("discord.js-commando");
-var MongoClient = require('mongodb').MongoClient;
-var MongoDBProvider = require('mongodb');
-var { initializeServices, services } = require("./services");
-var password = process.env.dbpassword;
+const path = require("path");
+const config = require("./localdata/config.json");
+const commando = require("discord.js-commando");
+const MongoClient = require('mongodb').MongoClient;
+const MongoDBProvider = require('mongodb');
+const { initializeServices, services } = require("./services");
+const log = require("fancy-log");
+const password = process.env.dbpassword;
+var utils = require("./utils");
 
 var client = new commando.Client({
 	owner: config.owners,
@@ -15,38 +17,54 @@ var client = new commando.Client({
 });
 
 client
-	.on("ready", () => {
-		console.log(`Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
+	.on("ready", async () => {
+		log(`Logged in as ${client.user.tag} (${client.user.id})`);
 		client.user.setActivity("with sapphires!");
+		// Initialize datahandler
+		client.datahandler = new utils.datahandler();
+		await client.datahandler.initialize();
+		log("Datahandler initialized.");
+		// Initialize services
+		await initializeServices(client);
+		log("Services initialized.");
+		// Attach to global object
+		global.services = services;
+		global.utils = utils;
+		log("Global variables initialized.");
+		// Register commands, groups and types
+		client.registry.registerDefaultTypes();
+		log("Default types initialized.");
+		client.registry.registerDefaultGroups();
+		log("Default groups initialized.");
+		client.registry.registerGroups([
+			["basics", "The essentials all bot needs."],
+			["fun", "Fun, time wasting commands."],
+			["utils", "General utility commands."],
+			["nadekoconnector", "Nadekoconnector commands."],
+			["challenges", "Commands for SBK Challenges."]
+		]);
+		log("Command groups initialized.");
+		client.registry.registerCommandsIn(path.join(__dirname, "commands"));
+		log("Commands initialized.");
 	})
 	.on("commandError", (cmd, err) => {
 		if (err instanceof commando.FriendlyError) return;
-		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+		log.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
 	})
 	.on("commandBlocked", (msg, reason) => {
-		console.log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
+		log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
 	})
 	.on("commandPrefixChange", (guild, prefix) => {
-		console.log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+		log(`Prefix ${prefix === "" ? "removed" : `changed to ${prefix || "the default"}`} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
 	})
 	.on("commandStatusChange", (guild, command, enabled) => {
-		console.log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
+		log(`Command ${command.groupID}:${command.memberName} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.
 		`);
 	})
 	.on("groupStatusChange", (guild, group, enabled) => {
-		console.log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
+		log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
 	});
-
-client.registry
-	.registerDefaultTypes()
-	.registerDefaultGroups()
-	.registerGroups([
-		["basics", "basic commands"],
-		["fun", "fun commands"],
-		["moderation", "moderation commands"]
-	])
-	.registerCommandsIn(path.join(__dirname, "commands"));
 
 client.login(process.env.BOT_TOKEN);
 initializeServices(client);
