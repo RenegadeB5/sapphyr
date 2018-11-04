ebc9ca29a06e2536bdb257e8b357c3157b0f3de2
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-04 15:35:59 -0500

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -31,26 +31,25 @@ module.exports = {
 				const query = { name: username };
 				console.log(await global.client.datahandler.fetchLink(query));
 				let result = global.client.datahandler.fetchLink(query)
-					if (result[result.length-1] === undefined) {
-						global.client.users.get(userid).send('Sorry, this invite link is no longer avalable.');
-						reaction.message.delete();
+				if (result[result.length-1] === undefined) {
+					global.client.users.get(userid).send('Sorry, this invite link is no longer avalable.');
+					reaction.message.delete();
+				}
+				else {
+					global.client.users.get(userid).send(result[result.length-1].link + '\nNotes:' + ' ' + result[result.length-1].notes);               
+					if (members.includes(username) === true) {
+						return;
 					}
 					else {
-						global.client.users.get(userid).send(result[result.length-1].link + '\nNotes:' + ' ' + result[result.length-1].notes);               
-						if (members.includes(username) === true) {
-							return;
+						let embed = new Discord.RichEmbed()
+						.setColor(0x0000FF)
+						.setTitle(username)
+						.addField('Party invite', result[result.length-1].notes)
+						.addField('Members', members + '\n' + username)
+						.setFooter('React with 🔗 to recieve the link, \nReact with ☠ if the link is invalid, \n And react with ⚠ if there is a troller present. \n Be aware that false alarms are punishable.')                  
+						reaction.message.edit({embed});  
 					}
-						else {
-							let embed = new Discord.RichEmbed()
-							.setColor(0x0000FF)
-							.setTitle(username)
-							.addField('Party invite', result[result.length-1].notes)
-							.addField('Members', members + '\n' + username)
-							.setFooter('React with 🔗 to recieve the link, \nReact with ☠ if the link is invalid, \n And react with ⚠ if there is a troller present. \n Be aware that false alarms are punishable.')                  
-							reaction.message.edit({embed});  
-						}
-					}
-				
+				}
 			}
 		}
 	}
