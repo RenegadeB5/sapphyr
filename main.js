0c07bca968050af94a4951e237227ba30902b8c7
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-03 21:54:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var { initializeServices, services } = require("./services");
 var password = process.env.dbpassword;
 
 var client = new commando.Client({
-	owner: config.bot.owners,
+	owner: config.owners,
 	commandEditableDuration: 0,
 	nonCommandEditable: false,
 	unknownCommandResponse: false,
@@ -15,33 +15,13 @@ var client = new commando.Client({
 });
 
 client
-	.on("ready", async () => {
-		console.log(`Logged in as ${client.user.tag} (${client.user.id})`);
+	.on("ready", () => {
+		console.log(`Logged in as ${client.user.username}#${client.user.discriminator} (${client.user.id})`);
 		client.user.setActivity("with sapphires!");
-		// Initialize services
-		await initializeServices(client);
-		console.log("Services initialized.");
-		// Attach to global object
-		global.services = services;
-		console.log("Global variables initialized.");
-		// Register commands, groups and types
-		client.registry.registerDefaultTypes();
-		console.log("Default types initialized.");
-		client.registry.registerDefaultGroups();
-		console.log("Default groups initialized.");
-		client.registry.registerGroups([
-			["basics", "basic commands"],
-			["fun", "fun commands"],
-			["utils", "utility commands"],
-			["nadekoconnector", "nadekoconnector commands"]
-		]);
-		console.log("Command groups initialized.");
-		client.registry.registerCommandsIn(path.join(__dirname, "commands"));
-		console.log("Commands initialized.");
 	})
 	.on("commandError", (cmd, err) => {
 		if (err instanceof commando.FriendlyError) return;
-		console.log.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
+		console.error(`Error in command ${cmd.groupID}:${cmd.memberName}`, err);
 	})
 	.on("commandBlocked", (msg, reason) => {
 		console.log(`Command ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ""} blocked. ${reason}`);
@@ -58,8 +38,19 @@ client
 		console.log(`Group ${group.id} ${enabled ? "enabled" : "disabled"} ${guild ? `in guild ${guild.name} (${guild.id})` : "globally"}.`);
 	});
 
+client.registry
+	.registerDefaultTypes()
+	.registerDefaultGroups()
+	.registerGroups([
+		["basics", "basic commands"],
+		["fun", "fun commands"],
+		["moderation", "moderation commands"]
+	])
+	.registerCommandsIn(path.join(__dirname, "commands"));
+
 client.login(process.env.BOT_TOKEN);
 initializeServices(client);
+global.services = services;
 global.client = client;
 global.MongoClient = MongoClient;
 global.MongoDBProvider = MongoDBProvider;
