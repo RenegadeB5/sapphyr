f540a9d6d7fdbcfe190e2ca4eb9ca9e5a9782586
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-04 13:47:00 -0500

    Update linkdetection.js

--- services/linkdetection.js
+++ services/linkdetection.js
@@ -2,6 +2,7 @@ var fs = require("fs");
 var regex = require("/app/links.json");
 var diepregex = RegExp(regex.diep);
 var Discord = require('discord.js');
+var dataHandler = global.client.datahandler();
 module.exports = {
 	name: "Link detection service",
 	description: "Creates an embed for party invites.",
