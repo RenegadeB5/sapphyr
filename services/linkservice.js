7e34518be31a78c393577a1814ace50402c7645b
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-07 12:37:36 -0500

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -81,7 +81,7 @@ module.exports = {
 				.setFooter('React with 🔗 to recieve the link,\nReact with ☠ if the link is invalid, \nAnd react with ⚠ if there is a troller present. \nBe aware that false alarms are punishable.');
 				linkchannel.send({embed}).then(function (message) {message.react('🔗')});
 				global.client.datahandler.insertLink(insert);
-				message.delete(1000);
+				message.delete();
 				message.channel.send('Your link has successfully been posted.').then(message => {message.delete(5000)});
 				setTimeout(clearLink, 3600000);
 			}
