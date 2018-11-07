7925fcd4f45aa68799fea2348146ba937aa79d12
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-07 18:32:31 -0500

    Update datahandler.js

--- utils/datahandler.js
+++ utils/datahandler.js
@@ -79,7 +79,7 @@ class dataHandler {
     }
     async fetchLink(query) {
         let links = await this.db.collection("partylinks");
-        return await links.find(query).sort({_id:1}).limit(1).toArray();
+        return await links.find(query).sort({_id:1}).limit(1).toArray()[0];
         
     }
     async remove1Link() {
