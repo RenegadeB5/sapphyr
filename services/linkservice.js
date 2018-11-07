a2b59276e395b284a2483e6dc1fbe28e00e3daf8
RenegadeB5 <rocketmanB5@gmail.com>
2018-11-07 18:34:52 -0500

    Update linkservice.js

--- services/linkservice.js
+++ services/linkservice.js
@@ -30,7 +30,6 @@ module.exports = {
 				let query = { name: username };
 				async function sendLink() {
 					let result = await global.client.datahandler.fetchLink(query);
-					console.log(result);
 					if (!result) {
 						global.client.users.get(userid).send('Sorry, this invite link is no longer avalable.');
 						reaction.message.delete();
